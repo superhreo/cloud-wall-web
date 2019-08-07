c06ba69018646dbf7e1a0117b70b0dab2af99ed3
bai-bai <2842401554@qq.com>
2019-08-07 22:11:23 +0800

    修改bug

--- src/components/itemDetail.vue
+++ src/components/itemDetail.vue
@@ -1,25 +1,8 @@
 <template>
     
     <!-- 内容的详情 -->
-    <div style="max-width:720px;" class="apper">
-        <a-row type="flex" justify="start" class="content">
-            <a-col :span="24">
-                {{ana.anaContent}}
-            </a-col>
-        </a-row>
-        <a-row type="flex" justify="start" class="form">
-            <a-col :span="24">
-                <span>{{ana.anaFrom}}</span>
-            </a-col>
-        </a-row>
-        <a-row type="flex" justify="start">
-            <a-col :span="24">
-                <a-row type="flex" justify="space-between" align="middle" class="other">
-                    <span class="otherSpan">« 物是人非</span>
-                    <span class="otherSpan">深渊 »</span>
-                </a-row>
-            </a-col>
-        </a-row>
+    <div>
+        
     </div>
 
 </template>
@@ -47,26 +30,4 @@ export default {
 </script>
 
 <style scoped>
-
-.content{
-    margin-bottom: 50px;
-    line-height: 32px;
-    margin: 50px 0px;
-    font-size: 16px;
-    color: #676F7A;
-}
-.form{
-    margin-bottom: 50px;
-    font-size: 16px;
-    color: #676F7A;
-}
-.other{
-    height: 50px;
-    border-top: 1px solid gainsboro;
-    border-bottom: 1px solid gainsboro;
-}
-.otherSpan:hover{
-    cursor: pointer;
-    color: cornflowerblue
-}
 </style>
--- src/components/itemList.vue
+++ src/components/itemList.vue
@@ -74,10 +74,6 @@ export default {
     methods:{
         //这里主要是我们在h5里面直接调方法，默认是调用该vue的，所以在这里声明一个变量，再将我们引用的放到这里面
         dateDiff:getDateDiff,
-        toItemDetail(){
-            //跳到语录详情页，并将当前的语录传过去
-            this.$router.push({name:'itemDetail',params:this.ana});
-        },
         onChange(pageNumber) {
         }
     }
