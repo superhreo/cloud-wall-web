938be9236daa3af08866c82e0ca3cd4a74957ea0
bai-bai <2842401554@qq.com>
2019-08-08 21:10:29 +0800

    ...

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,14 +26,6 @@
 <script>
 import { getDateDiff } from '../utils/date'
 export default {
-    methods:{
-        //这里主要是我们在h5里面直接调方法，默认是调用该vue的，所以在这里声明一个变量，再将我们引用的放到这里面
-        dateDiff:getDateDiff,
-        toItemDetail(){
-            //跳到语录详情页，并将当前的语录传过去
-            this.$router.push({name:'itemDetail',params:this.ana});
-        }
-    }
 }
 </script>
 
